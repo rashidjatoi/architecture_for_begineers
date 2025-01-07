c708185d5fe7d001b635984a3a8e9433ae3a0d05
rashidjatoi <rashidjatoi31@gmail.com>
2025-01-07 18:28:28 +0500

    added dark mode for me redability

--- lib/main.dart
+++ lib/main.dart
@@ -34,12 +34,8 @@ class MyApp extends StatelessWidget {
       ],
       child: MaterialApp(
         title: 'Flutter Demo',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(
-            seedColor: Colors.deepPurple,
-          ),
-          useMaterial3: true,
-        ),
+        themeMode: ThemeMode.dark,
+        darkTheme: ThemeData.dark(),
         home: const HomeView(),
       ),
     );
